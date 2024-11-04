ce79bedf9953a4a0dacd046f012cd32d038177e7
Abhinandan Patil <abhinandanpatil002@gmail.com>
2024-11-04 14:32:54 +0530

    Added Assignments

--- Assignment4/AssignmentCircle.sql
+++ Assignment4/AssignmentCircle.sql
@@ -1,21 +1,25 @@
 
-create table area2(
+create table areacal(
     radius int,
     areacal float
 );
 
 
 DELIMITER &&
-CREATE OR REPLACE procedure calarea(in rad INT)
-BEGIN 
-DECLARE area FLOAT DEFAULT 0;
-if rad<5 or rad >9 THEN 
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Invalid radius (select value between 5 to 9 )";
-else 
-    SET area=3.14*rad*rad;
-    INSERT INTO calareaofcircle VALUES (rad,area);
-END if;
+
+CREATE OR REPLACE PROCEDURE areaofCircle2(IN rad INT)
+BEGIN
+    DECLARE area FLOAT;
+
+    IF (rad < 5) OR (rad > 9) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid Radius';
+        -- SELECT "Enter valid r" as message;
+    ELSE
+        SET area = 3.14 * rad * rad;
+        INSERT INTO areacal VALUES (rad, area);
+    END IF;
 END &&
+
 DELIMITER ;
 
 
